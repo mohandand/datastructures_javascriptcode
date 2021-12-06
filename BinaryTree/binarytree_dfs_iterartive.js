6520094c9c75b9e3b4b783535b4b72cd7006fe2e
mohandand <mohandand@gmail.com>
2021-12-06 15:16:07 -0800

    updated code

--- BinaryTree/binarytree_dfs_iterartive.js
+++ BinaryTree/binarytree_dfs_iterartive.js
@@ -15,7 +15,7 @@ const d = new Node('d');
 const e = new Node('e');
 const f = new Node('f');
 
-//By doing below assignements i am connecting nodes together
+//By doing below assignements I am connecting nodes together
 a.left = b;
 a.right = c;
 b.left = d;
