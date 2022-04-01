3999dc56c6cba4369e8e1d937547102ce3e9d82b
mohandand <mohandand@gmail.com>
2022-04-01 14:35:20 -0700

    Practising Code

--- BinaryTree/class.js
+++ BinaryTree/class.js
@@ -1,16 +1,41 @@
 class Node{
-    constuctor(val){
-        this.node=val;
+    constructor(val){
+        this.val=val;
         this.left=null;
-        this.right=null
+        this.right=null;
     }
 }
 
 const a = new Node('a');
-const b = new  Node('b');
+const b = new Node('b');
 const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
 
-a.left = b
-a.right =c
+//By doing below assignements I am connecting nodes together as below
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.left = f;
 
-console.log(a.left)
+
+function dfs(root){
+    if(root == null) return [];
+    let stack = [root];
+    let result = [];
+    while(stack.length>0){
+        let current = stack.pop();
+        result.push(current.val);
+        if(current.right){
+            stack.push(current.right)
+        }
+        if(current.left){
+            stack.push(current.left)
+        }
+    }
+    return result
+}
+
+console.log(dfs(a))
