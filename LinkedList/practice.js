0a017a5ac901982890db8ae6d6f92b29d84f3a9b
mohandand <mohandand@gmail.com>
2022-04-03 12:04:25 -0700

    Practising Coding

--- LinkedList/practice.js
+++ LinkedList/practice.js
@@ -1,16 +1,6 @@
-function anagrams(word, words) {
-    if(word === "" || words === []) return []
-    let res=[]
-    let arryword = word.split('').sort().join('');
-    for(let i=0;i<=words.length;i++){
-      let sortword = words[i].split('').sort().join('');
-      console.log(arryword)
-      console.log(sortword)
-      if(arryword===sortword){
-        res.push(words[i])
-      }
-    }
-    return res
-  }
-
-  console.log('racer',['crazer', 'carer', 'racar', 'caers', 'racer'])
+function incrementString (string) {  
+    // Extract string's number
+    var number = string.match(/\d+/) === null ? 0 : string.match(/\d+/)[0];
+    console.log(number)
+}
+console.log(increamentString(number))
