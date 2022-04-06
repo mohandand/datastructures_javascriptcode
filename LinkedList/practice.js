0bbf90aae70855f8b6b1e594fa7e45b00b37f833
mohandand <mohandand@gmail.com>
2022-04-05 23:07:45 -0700

    Practising Coding

--- LinkedList/practice.js
+++ LinkedList/practice.js
@@ -1,10 +1,21 @@
 function scramble(str1, str2) {
-  for(let char of str2){
-    if(!str2.includes(char)){
-      return false
-    }
+  let obj1={}
+  let obj2 = {}
+  for(let char of str1){
+    obj1[char]?obj1[char]++:obj1[char]=1
   }
+  console.log(obj1)
+   for(let char of str2){
+     obj2[char]?obj2[char]++:obj2[char]=1
+   }
+   console.log(obj2)
+   for(item in obj2){
+     if(!(obj2[item] == obj1[item])){
+        return false
+        }
+   }
    return true
  }
  
- console.log(scramble("abc" ,"abca"))
+ 
+ console.log(scramble("rkqodlw" ,"world"))
