1614dc7571ca83b364198e3e97ff3021ca4f6bbb
mohandand <mohandand@gmail.com>
2022-04-22 21:50:32 -0700

    Practicing Code

--- LinkedList/practice.js
+++ LinkedList/practice.js
@@ -1,18 +1,25 @@
-var countSubstrings = function(s) {
-  let count=0;
-  const arr=[];
-  for(let i=0;i<s.length;i++){
-      countPalindrome(s,i,i); // odd length
-      countPalindrome(s,i,i+1); // even length
-  }
-  return count;
-  function countPalindrome(str,left,right){
-      while(left>=0 && right<str.length && str[left]===str[right]){
-    arr.push(str.substring(left,right+1)); // if you want those substrings as well
-          count++;
-          left--;
-          right++;
+var setZeroes = function (matrix) {
+  let index = []
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[0].length; c++) {
+      if (matrix[r][c] == 0) {
+        index.push([r, c])
       }
+    }
   }
+  for (let address of index) {
+    let row = address[0]
+    let col = address[1]
+    fix(row, col, matrix)
+  }
+  return matrix
 };
-console.log(countSubstrings("abba"))
+
+const fix = (r, c, matrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i][c] = 0
+  }
+  for (let i = 0; i < matrix[0].length; i++) {
+    matrix[r][i] = 0
+  }
+}
