156a424b4a8bce2017a2d40540ff3400048e6a79
Mohan Dandigam <mohandand@gmail.com>
2022-05-29 20:42:13 -0700

    Linked List Program

--- LinkedList/practice.js
+++ LinkedList/practice.js
@@ -1,6 +1,10 @@
-class Node{
-  constructor(data,next=null){
-    this.data=data;
-    this.next = next;
+function partsSums(ls) {
+  let lsr = [];
+let sum = 0;
+  for(let i =ls.length-1 ;i>=-1;i--){
+     lsr.unshift(sum)
+      sum = sum + ls[i]
   }
+return lsr;
 }
+console.log(partsSums([1, 2, 3, 4, 5, 6]))
---
+++ LinkedList/sumLinkedList.js
@@ -0,0 +1,36 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next=null;
+    }
+}
+const a  = new Node(2)
+const b  = new Node(3)
+const c  = new Node(2)
+const d  = new Node(3)
+
+a.next = b;
+b.next = c
+c.next = d
+
+
+function sumOfLLValues(head){
+    let current = head;
+    let sum =0;
+    while(current != null){
+        sum+=current.val;
+        current = current.next;
+    }
+    return sum;
+}
+
+
+
+var sum=0;
+function recursiveSum(head){
+    if(head == null) return;
+    sum+=head.val
+    return recursiveSum(head.next)
+}
+
+recursiveSum(a);
