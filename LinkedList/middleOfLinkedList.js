a6dc8aa28d9a25f3c57488b4efb81597a7289946
Mohan Dandigam <mohandand@gmail.com>
2022-06-03 13:14:41 -0700

    Linked List program

---
+++ LinkedList/middleOfLinkedList.js
@@ -0,0 +1,11 @@
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    while(fast ){
+        if(!fast.next) return slow
+        slow = slow.next;
+        fast = fast.next.next
+    }
+    
+    return slow
+};
