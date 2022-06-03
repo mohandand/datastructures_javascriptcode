2323a9a86789b23b622a77a65959f92322cf176f
Mohan Dandigam <mohandand@gmail.com>
2022-06-03 11:24:46 -0700

    Linked List program

---
+++ LinkedList/findnodeAtCycleStart.js
@@ -0,0 +1,8 @@
+//approach: two pointer (fast and slow pointer)
+//it is linked list problem
+
+//similar concept to detecting the cycle, 
+//but after we find the cycle, as floyd says, 
+//move back the slow pointer to the beginning 
+//and then move slow and fast 1x speed, 
+//whereever fast and slow meet then, will be beginning of the loop
