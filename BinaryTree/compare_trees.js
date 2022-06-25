818c1da4cc8b8d078b04a40f306a75d7732e6824
Mohan Dandigam <mohandand@gmail.com>
2022-06-24 20:16:11 -0700

    Binary Tree Expalantion

--- BinaryTree/compare_trees.js
+++ BinaryTree/compare_trees.js
@@ -9,3 +9,29 @@ const isSameTree = (p, q) => {
     // otherwise return recursively the right nodes & left nodes
     return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
 };
+
+//Iterartive
+
+var isSameeTree = function(p, q) {
+    return recursive(p, q);
+    return iterative(p, q);  
+};
+
+function iterative(p, q) {
+    const queue = [p, q];
+    while (queue.length) {
+        const node1 = queue.shift();
+        const node2 = queue.shift();
+        if (!node1 && !node2) {
+            continue;
+        }
+        if ((!node1 && node2) || (node1 && !node2) || node1.val !== node2.val) {
+            return false
+        }
+        queue.push(node1.left);
+        queue.push(node2.left);
+        queue.push(node1.right);
+        queue.push(node2.right);   
+    } 
+     return true;   
+}
