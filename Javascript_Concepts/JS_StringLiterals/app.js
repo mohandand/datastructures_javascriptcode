422e6e13d7432bb5a9697a23e22f2c828a99c20e
Mohan Dandigam <mohandand@gmail.com>
2022-07-13 14:35:38 -0700

    JS Coding Practise

--- Javascript_Concepts/JS_StringLiterals/app.js
+++ Javascript_Concepts/JS_StringLiterals/app.js
@@ -6,7 +6,7 @@ let list = `<div>
  <li>Item-1</li>
 </ul>
 </div>`
-//Yes, setting the .innerHTML properties overwrites any previous value for that property.
+//setting the .innerHTML properties overwrites any previous value for that property.
 
 /*If you want to append your info to it, you have to code it to append by adding to the previous HTML, not overwriting the previous value (note the use of += here).
 
@@ -16,8 +16,36 @@ list is created with string not as Node so you cant use parent.appendChild(list)
 parent.innerHTML = parent.innerHTML + list;
 
 
+const dogs = [
+    { name: 'Snickers', age: 2 },
+    { name: 'Hugo', age: 8 },
+    { name: 'Sunny', age: 1 }
+]
+
+const dogList = `<ul> 
+${dogs.map(dog => {
+    <li>${dog.name} has age of ${dog.age}</li>
+})}
+</ul>`
+
+parent.innerHTML = parent.innerHTML + dogList;
+
+
+
+//In JavaScript DOM, you can choose to only fire an event once. 
+//This means you can allow an event to only be triggered once on a specific element.
 let button = document.getElementsByTagName('button')[0];
 
 button.addEventListener('click', () => {
-alert('Hello There!')
-} , )
+console.log('Hello There!')
+},{once:true} )
+
+/* If you want to add an event callback but have it run only once, you can simply use the once option in the method's options object:
+This prevents the need to otherwise immediately remove the event listener after the callback first fires (which I've been guilty of!):
+
+document.body.addEventListener('click', cb);
+
+  function cb() {
+    console.log('Hi! 👋');
+    document.body.removeEventListener('click', cb);
+  }*/
