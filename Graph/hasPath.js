05c204023c4f8c559ae62db6d14e165c54ff690b
Mohan Dandigam <mohandand@gmail.com>
2022-07-18 12:19:30 -0700

    JS Coding Practise

---
+++ Graph/hasPath.js
@@ -0,0 +1,25 @@
+//DFS Solution
+function hasPath(graph,src,des){
+    if(src===des) return true;
+
+    for(let neighbour of graph[src]){
+        if(hasPath(graph,neighbour,des) === true){
+            return true;
+        }
+    }
+   return false;
+}
+
+//BFS Iterative Soultion
+function hasPathBFS(graph,src,des){
+    const queue = [src];
+    while(queue>0){
+        let current = queue.shift();
+        if(current === des) return true
+        for(let neighbor of graph[src]){
+            queue.push(neighbor)
+        }
+    }
+    return false;
+}
+
