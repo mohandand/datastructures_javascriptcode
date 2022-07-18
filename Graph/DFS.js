25a8d3ef6bb8524cc33205ee9d54a809c34de33d
Mohan Dandigam <mohandand@gmail.com>
2022-07-17 23:22:04 -0700

    JS Coding Practise

--- Graph/DFS.js
+++ Graph/DFS.js
@@ -13,7 +13,6 @@ const depthFirst = (graph,source) => {
     while(stack.length>0){
         const current = stack.pop();
         console.log(current)
-
         for(let neighbor of graph[current]){
          stack.push(neighbor)    
         }
@@ -22,5 +21,10 @@ const depthFirst = (graph,source) => {
 }
 
 //Recursive Approach
+
+const dfsRecursive = (graph,source){
+   if(stack.length == 0) return;
+   
+}
 
 console.log(depthFirst(graph,'a')) //abdfce
