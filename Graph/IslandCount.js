1067c579ae3eb616ab122925a038a572103fefcc
Mohan Dandigam <mohandand@gmail.com>
2022-07-19 15:04:09 -0700

    JS Coding Practise

--- Graph/IslandCount.js
+++ Graph/IslandCount.js
@@ -4,8 +4,8 @@ const islandCount = (grid) => {
  //loop through grid
  for(let row=0;row<grid.length;row++){
     for(let col = 0;col<grid[0].length;col++){
-        if((explore(grid,row,col),visited) == true){
-            count++
+        if(explore(grid,row,col,visited) === true){
+            count = count+1
         };
     }
  }
@@ -13,13 +13,13 @@ const islandCount = (grid) => {
 }
 
 const explore = (grid,r,c,visited) => {
- const rowInbounds = 0<r && r<grid.length;
- const colInbounds = 0<c && c<grid.length;
- if(rowInbounds || colInbounds) return false;
+ const rowInbounds = 0<=r && r<grid.length;
+ const colInbounds = 0<=c && c<grid.length;
+ if(!rowInbounds || !colInbounds) return false;
 
  if(grid[r][c] == 'w') return false
  
-   const pos = r+','+c;
+   const pos = r + ',' + c;
    if(visited.has(pos)) return false;
    visited.add(pos);
 
@@ -29,3 +29,14 @@ const explore = (grid,r,c,visited) => {
    explore(grid,r,c+1,visited)
    return true;
 }
+
+const grid = [
+    ['w','l','w','w','w'],
+    ['w','l','w','w','w'],
+    ['w','w','w','l','w'],
+    ['w','w','l','l','w'],  
+    ['l','w','w','l','l'],
+    ['l','l','w','w','w'],
+]
+
+console.log(islandCount(grid))
