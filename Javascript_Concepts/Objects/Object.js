a8411d4c510f2e943646127891f97e9a700630a2
Mohan Dandigam <mohandand@gmail.com>
2022-07-24 20:57:39 -0700

    Coding practise

--- Javascript_Concepts/Objects/Object.js
+++ Javascript_Concepts/Objects/Object.js
@@ -53,12 +53,15 @@ const a = {};
 const  b = {key:"b"}
 const  c = {key:"c"}
 a[b]=123; // when we assign key as object it assignd like a["[Object Object]"] =123 
-          //as object cant be converted as key unless its string when it tries to convert it becomes Object Object;
+          //as object cant be converted as key unless its string when it tries to convert forcefully to string it will becomes Object Object;
 a[c]=456; // when we assign key as object it assignd like a["[Object Object]"] =456 so overirided;
 console.log(a)
 console.log(a[b])
 
 //JSON.stringfy() and JSON.parse()
+// we use this In API calls as they accept String not objct and we cant directly store object inside local storage so we nned to convert
+
+
 
 const user2 = {
     name:"Mohan",
@@ -67,7 +70,30 @@ const user2 = {
 
 let strObj = JSON.stringify(user2) //convert as String
 
+//localStorage.setItem(test ,strObj) // key will be test and object as string willbe value. caheck under aplications if u asssign object directly it give [Object Object] as when it tries to convert forcefully to string it will becomes Object Object;
+
 console.log( strObj,typeof strObj)  /
 
 
 console.log( JSON.parse(strObj),typeof JSON.parse(strObj))   //Convert Back to Object
+
+//Spread operator 
+//with string will spread all charcters 
+
+console.log([..."MOHAN"])
+
+const user3 = {
+    name:"Mohan",
+    age:27,
+}
+
+const user4 = {
+    namef:"Mohan",
+    agef:27,
+    ...user3  //add user3 all properties insidd user4
+}
+
+console.log(user4)
+
+
+
