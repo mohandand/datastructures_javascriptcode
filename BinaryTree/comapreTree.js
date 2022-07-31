e9f6bdb7de0ec0d33eb1bc2f304c5b5acbec8c65
Mohan Dandigam <mohandand@gmail.com>
2022-07-31 16:23:26 -0700

    Coding Practice

--- BinaryTree/comapreTree.js
+++ BinaryTree/comapreTree.js
@@ -9,9 +9,10 @@ class Node{
 const a =new Node('a')
 const b = new Node('b')
 const c = new Node('c')
-
 a.left = b
 a.right = c
+b.left = c
+b.right = b
 
 function isSame(p,q){
     if(!p && !q) return true;
@@ -22,4 +23,26 @@ function isSame(p,q){
     return left&&right;
 }
 
-console.log(isSame(a,a));
+function isSameRec(p,q){
+    let queue = [p,q];
+    while(queue.length>0){
+        let node1 = queue.shift();
+        let node2 = queue.shift();
+
+        if(!node1 && !node2){
+            continue;
+        }
+
+        if((!node1 || !node2) && node1 !== node2){
+            return false;
+        }
+        queue.push(node1.left)
+        queue.push(node2.left)
+        queue.push(node1.right)
+        queue.push(node2.left)
+    }
+}
+
+
+
+console.log(isSameRec(a,a));
