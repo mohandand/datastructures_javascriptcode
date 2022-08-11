8a4727c62482394707602c931c3f9d9b485fca6f
Mohan Dandigam <mohandand@gmail.com>
2022-08-11 12:57:55 -0700

    HTML and CSS Comleted

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(array){
+    
+    for(let i=0;i<array.length;i++){
+        let swap = true;
+        for(let j=1;j<array.length-i;j++){
+            if(array[j]>array[j-1]){
+                let temp =array[j-1];
+                array[j-1] =array[j];
+                array[j] = temp;
+                swap =false;
+            }
+        }
+        if(swap){
+            break;
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSort([1,4,2,5,3]))
