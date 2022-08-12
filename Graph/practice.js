01974a3f5cb6d6ed227f8b324fe8883de6778620
Mohan Dandigam <mohandand@gmail.com>
2022-08-11 18:06:52 -0700

    HTML and CSS Comleted

--- Graph/practice.js
+++ Graph/practice.js
@@ -1,20 +1,8 @@
-
-/* 
-Declare a variable for our current overall maximum sum
-Declare a temporary sum for comparison
-Iterate over nums starting at nums[1] since we set our initial sums to nums[0]
-The temporary sum is either the current number if the prior tempSum was negative for example
-Or it is the sum of tempSum and the new sum if that figure is larger 
-Every time we have a new tempSum we want to compare it to our running maxSum 
- */
-
-var maxSubArray = function(nums) {
-    let maxSum = nums[0];   
-	let sum = nums[0];
-	for(let i=1; i< nums.length; i++){   // start with 2nd element.
-		sum = Math.max(nums[i], nums[i]+sum);
-		if(sum > maxSum) maxSum = sum;
-	}
-	return maxSum;
+var findMin = function(nums) {
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1]>nums[i]){
+            return nums[i]
+        }
+    }
+    return nums[0]
 };
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
